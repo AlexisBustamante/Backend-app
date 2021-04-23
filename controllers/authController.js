add805e85d45286b1ba13524f9bbffe93655c658
alexis bustamante <alexisbustamantecisternas@gmail.com>
2021-04-23 16:46:14 -0400

    ingreso de usuario

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,40 @@
-import pool from '../database/keys';
+import pool from "../database/keys";
 
 const authentication = {};
 
 authentication.signIn = async (req, res) => {
-    
-    const {saludo,nombre} =req.body;
-  
-   const clientes= await(await pool.query("SELECT * FROM usuarios ")).rows;
+  const { email, password } = req.body;
 
-   console.log(clientes);
+  //console.log(req.body);
 
+  try {
+    const usuario = await (
+      await pool.query(
+        "SELECT * FROM usuarios where usr_email=$1 AND usr_password=$2",
+        [email, password]
+      )
+    ).rows;
 
+    if (usuario.length > 0) {
+      res.status(200).json({
+          
+            id_usr:usuario[0].id_usr,
+            usr_name:usuario[0].usr_name,
+            usr_email:usuario[0].usr_email
+        })
+    } else {
+      res.status(400).json({
+        message: "Usuario no Existe o la contraseña es incorrecta",
+        NotFound: true,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "ocurrio un error",
+      error,
+    });
+  }
 };
 
 module.exports = authentication;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,5 @@ import express from 'express';
 import auth from '../controllers/authController';
 
 const router = express.Router();
-
 router.post('/signIn',auth.signIn);
-
 module.exports = router;
